f757e031f6b7be090f04d5ce8ce95123cc0033b6
Abdelrhman <abdelrhman012822@gmail.com>
2024-09-10 14:51:07 +0300

    Project about phone

---
+++ phone .js
@@ -0,0 +1,22 @@
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let bigimg = document.getElementById("bigimg");
+let body = document.getElementById("my_body");
+function img11() {
+  bigimg.setAttribute("src", "abdelrahman gamal/1.png");
+  body.style.backgroundColor = "#26a4ee";
+}
+function img12() {
+  bigimg.setAttribute("src", "abdelrahman gamal/2.png");
+  body.style.backgroundColor = "#534d4d";
+}
+function img13() {
+  bigimg.setAttribute("src", "abdelrahman gamal/3.png");
+  body.style.backgroundColor = "#c8ca83";
+}
+function img14() {
+  bigimg.setAttribute("src", "abdelrahman gamal/4.png");
+  body.style.backgroundColor = "#4c0508";
+}
